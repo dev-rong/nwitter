46a13447c015222d1516fe3e930004c9e703d750
dev-rong <dev.ronggg@gmail.com>
2022-02-27 00:00:32 +0900

    Update Auth.js

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,23 +1,37 @@
 import {useState} from "react";
 import React from "react";
+import {
+    getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword,
+    } from 'firebase/auth';
 
 const Auth = () => {
     
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-    const onChange=(e)=>{
+    const [newAccount, setNewAccount] = useState(true);
+    const auth = getAuth();
+    
+    const onChange = (e) =>{
         const {target:{name, value}} = e;
-
-        if(name==="email"){
+        if(name === "email"){
             setEmail(value)
-        } else if(name==="password"){
+        } else if(name === "password"){
             setPassword(value)
         }
     }
 
-    const onSubmit=(e)=>{
+    const onSubmit = async (e) =>{ 
         e.preventDefault();
+        try {
+            if (newAccount) {
+                const data = await createUserWithEmailAndPassword(auth, email, password);
+            } else {
+                const data = await signInWithEmailAndPassword(auth, email, password);
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
 
     return(
@@ -30,7 +44,7 @@ const Auth = () => {
             <input type="password" name="Password" 
             value={password} onChange={onChange} 
             placeholder="password" required/>
-            <input type="submit" value="Log In"/>
+            <input type="submit" value={newAccount? "Create Account": "Log In"}/>
             
         </form>    
             <div>
