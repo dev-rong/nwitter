0a7ee30b9c216fd5e0be9d9c28f43bbfacf35857
dev-rong <dev.ronggg@gmail.com>
2022-03-08 23:52:09 +0900

    Add firebase functions

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    upvotedOn: [],
+  });
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+    const doc = admin.firestore().collection('users').doc(user.uid);
+    return doc.delete();
+  });
